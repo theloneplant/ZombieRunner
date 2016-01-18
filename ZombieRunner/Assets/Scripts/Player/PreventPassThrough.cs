fcbd5f9c0db40f34fc3d08aad9a1bdca7f263405
theloneplant <theloneplant@gmail.com>
2016-01-17 20:51:30 -0800

    Player collision now uses raycasting to prevent pass through

---
+++ ZombieRunner/Assets/Scripts/Player/PreventPassThrough.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreventPassThrough : MonoBehaviour
+{
+    public LayerMask layerMask;
+
+    private BoxCollider2D hitBox;
+    private Rigidbody2D rb;
+    private Vector2 prevPosition;
+    
+    void Start()
+    {
+        hitBox = GetComponent<BoxCollider2D>(); // Add more colliders in the future
+        rb = GetComponent<Rigidbody2D>();
+        prevPosition = rb.position;
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rb.position, -Vector2.up, Mathf.Infinity, layerMask);
+        if (hit.collider != null)
+        {
+            // Use previous velocity to see if the object will pass through a collider
+            float yDelta = rb.position.y - prevPosition.y;
+            if (hit.distance < Mathf.Abs(yDelta) && yDelta < 0 && hit.distance > 0)
+            {
+                // There will be a collision next frame, so set the object on top of the collider
+                rb.position = new Vector2(hit.point.x, hit.point.y);
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+            }
+        }
+
+        // Update previous position
+        prevPosition = rb.position;
+    }
+}
